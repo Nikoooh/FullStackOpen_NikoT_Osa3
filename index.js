8fab1f8b03b517b764a57fba449cdaca5faafcc4
Nikoooh <nikotoivanen1999@gmail.com>
2024-01-18 11:53:29 +0200

    3.1-3.6 tehty

---
+++ index.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let phonebook = [
+    {
+      "name": "Arto Hellas",
+      "number": "040-123456",
+      "id": 1
+    },
+    {
+      "name": "Ada Lovelace",
+      "number": "39-44-5323523",
+      "id": 2
+    },
+    {
+      "name": "Dan Abramov",
+      "number": "12-43-234345",
+      "id": 3
+    },
+    {
+      "name": "Mary Poppendieck",
+      "number": "39-23-6423122",
+      "id": 4
+    },
+]
+
+app.get('/api/persons', (req, res) => {
+    res.status(200).json(phonebook)
+})
+
+app.get('/info', (req, res) => {
+
+    const currentDate = new Date()
+
+    res.send(`
+    <p>Phonebook has info for ${phonebook.length} people</p> 
+    <p> ${currentDate} </p>
+    `)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const person = phonebook.find((person) => person.id === Number(req.params.id))
+    
+    if(person) {
+        res.status(200).json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    phonebook = phonebook.filter((person) => person.id !== Number(req.params.id))
+    res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+    const newId = Math.floor(Math.random() * 500000)
+
+    const newPerson = {
+        name: req.body.name,
+        number: req.body.number,
+        id: newId
+    }
+
+    if (!req.body.name || !req.body.number) {
+        res.status(400).json({error: "Please input name and number"})
+    } else if (phonebook.find((person) => person.name === req.body.name)) {
+        res.status(400).json({error: "Name must be unique"})
+    } else {
+        phonebook = phonebook.concat(newPerson)
+        res.status(200).json(phonebook)
+    }
+
+})
+
+const port = 3001
+app.listen((port), () => {
+    console.log(`Server running in port ${port}`);
+})
